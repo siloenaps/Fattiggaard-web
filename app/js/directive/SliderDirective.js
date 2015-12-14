cfddf0e4f7021a1243fc198b70eb4810c441fc8d
siloenaps <str@siloen.dk>
2015-12-14 17:44:23 +0100

    test

--- app/js/directive/SliderDirective.js
+++ app/js/directive/SliderDirective.js
@@ -8,21 +8,27 @@ module.exports = function($timeout) {
 	// scope.imagesTotal: scope.images.length,
     link: function (scope, elem, attrs) {
 	
-		scope.currentIndex=0;
+		scope.currentIndex = 0;
 
 		scope.next=function(){
+			scope.direction = 1;
 			(scope.currentIndex < scope.images.length - 1) ? scope.currentIndex++ : scope.currentIndex = 0;
 		};
 		
 		scope.prev=function(){
+			scope.direction = -1;
 			(scope.currentIndex > 0) ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
 		};
 		
-		scope.$watch('currentIndex',function(){
+		scope.$watch('currentIndex',function(){			
 			scope.images.forEach(function(image){
 				image.visible = false;
 			});
 			scope.images[scope.currentIndex].visible = true;
+			console.log(scope.images[scope.currentIndex])
+			// TweenLite.to(scope.images[scope.currentIndex], 1, { left: 512 * scope.direction, onComplete: function(){
+	  //         console.log('done');
+	  //       } });
 		});
 		
 		/* Start: For Automatic slideshow*/
